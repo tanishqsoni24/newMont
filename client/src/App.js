bf931aa12cd39674711ef0da5c0dc73dbc602b10
Shubh Chaturvedi <chaturvedishubh650@gmail.com>
2023-10-05 14:26:35 +0530

    DEV: login

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import React from 'react';
 import Home from './pages/Home';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <React.Fragment>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
