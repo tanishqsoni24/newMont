1e48818ada2ed977fa958df8c0d0ea52d3066f79
Shubh Chaturvedi <chaturvedishubh650@gmail.com>
2023-10-05 12:57:31 +0530

    DEV: Tailwind setup and navbar

--- client/src/components/general/Crousel.jsx
+++ client/src/components/general/Crousel.jsx
@@ -7,8 +7,8 @@ export default function Crousel() {
     <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
             <img src="/banner.jpeg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-        </div>
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
+        </div >
+        <div className="something hidden duration-700 ease-in-out" data-carousel-item>
             <img src="/banner.jpeg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
         </div>
         <div className="hidden duration-700 ease-in-out" data-carousel-item>
