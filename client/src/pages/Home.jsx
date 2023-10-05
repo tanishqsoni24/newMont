09f7905ee30059bdceacbd4a54ea2ddede1da871
Shubh Chaturvedi <chaturvedishubh650@gmail.com>
2023-10-05 12:50:45 +0530

    DEV: Tailwind setup and navbar

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/global/Navbar'
+import Crousel from '../components/general/Crousel'
+
+export default function Home() {
+  return (
+    <React.Fragment>
+        <Navbar />
+        <Crousel/>
+    </React.Fragment>
+  )
+}
