7a593fc5a80304f6726509ea1337516c24cc63bf
RythamSharma <sharmarytham2@gmail.com>
2023-10-22 12:46:52 +0530

    navbar updated and home page updated

--- client/src/pages/BankCard.jsx
+++ client/src/pages/BankCard.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import { useState } from 'react'
-import Cookies from 'js-cookie'
-import jwt_decode from 'jwt-decode'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 export default function BankCard() {
   const [bankCard, setBankCard] = useState([]);
@@ -16,35 +16,48 @@ export default function BankCard() {
         phone_number: decoded.phone_number
       }, { headers: { 'Content-Type': 'application/json' } });
       if (response.data.status === "Success") {
-        console.log(response.data.data)
-        setBankCard(response.data.data)
+        console.log(response.data.data);
+        setBankCard(response.data.data);
       }
-    }
-    showmybankcard()
-  } , [])
+    };
+    showmybankcard();
+  }, []);
   return (
-    <div style={{ marginTop: "7rem" }} className="container flex flex-col mx-auto justify-center item-center">
-            <div className="dark:bg-gray-800 dark:border-gray-700 flex justify-around">
+    <div
+      style={{ marginTop: "7rem" }}
+      className="container flex flex-col mx-auto justify-center item-center"
+    >
+      <div className="dark:bg-gray-800 dark:border-gray-700 flex justify-around">
+        <div className="withdrawals flex flex-col item-center justify-center">
+          <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            Bank Card
+          </h2>
+          {bankCard.length > 0 &&
+            bankCard.map((bankCard, index) => {
+              return (
+                <div className="max-w-sm w:full-screen my-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <span className="font-bold">Card Holder Name:</span>{" "}
+                    {bankCard.card_holder_name} <br />
+                    <span className="font-bold">Bank Name:</span>{" "}
+                    {bankCard.bank_name} <br />
+                    <span className="font-bold">Card Number:</span>{" "}
+                    {bankCard.card_number} <br />
+                    <span className="font-bold">IFSC Code:</span>{" "}
+                    {bankCard.ifsc_code} <br />
+                  </p>
+                </div>
+              );
+            })}
 
-
-                <div className="withdrawals flex flex-col item-center justify-center">
-                    <h2 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>Bank Card</h2>
-                    { bankCard.length>0 && bankCard.map((bankCard, index) => {
-                      return (
-<div className="max-w-sm w:full-screen my-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        <span className="font-bold">Card Holder Name:</span> {bankCard.card_holder_name} <br />
-        <span className="font-bold">Bank Name:</span> {bankCard.bank_name} <br />
-        <span className="font-bold">Card Number:</span> {bankCard.card_number} <br />
-        <span className="font-bold">IFSC Code:</span> {bankCard.ifsc_code} <br />
-
-    </p>
-</div>
-                      )})}
-
-                    <Link to="/add-card" className="focus:outline-none my-3 text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Add Card</Link>
-                    </div>
-                    </div>
-                    </div>
-  )
+          <Link
+            to="/add-card"
+            className="focus:outline-none my-3 text-white bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+          >
+            Add Card
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,154 +1,214 @@
-import React,{useState, useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import Cookies from "js-cookie";
-import axios from 'axios'
-
-
+import axios from "axios";
 
 export default function Team() {
-    const [user, setUser] = useState({
-        invite_code: '',
-    })
+  const [user, setUser] = useState({
+    invite_code: "",
+  });
 
-    const [level_1, setLevel_1] = useState([]);
-    const [level_2, setLevel_2] = useState([]);
-    const [level_3, setLevel_3] = useState([]);
+  const [level_1, setLevel_1] = useState([]);
+  const [level_2, setLevel_2] = useState([]);
+  const [level_3, setLevel_3] = useState([]);
 
-    const [team_size, setTeam_size] = useState(0);
-    const [team_recharge, setTeam_recharge] = useState(0);
-    const [withdrawls, setWithdrawls] = useState(0);
-    useEffect(() => {
-        const userDeatil = async () => {
-        const token = Cookies.get("session_id");
-        const decoded = await jwt_decode(token);
-        console.log(decoded)
+  const [team_size, setTeam_size] = useState(0);
+  const [team_recharge, setTeam_recharge] = useState(0);
+  const [withdrawls, setWithdrawls] = useState(0);
+  useEffect(() => {
+    const userDeatil = async () => {
+      const token = Cookies.get("session_id");
+      const decoded = await jwt_decode(token);
+      console.log(decoded);
 
-        setUser({
-            invite_code: decoded.invite_code,
-        })
-        const response = await axios.post('http://139.59.32.207/accounts/my-teams/',{phone_number : decoded.phone_number} ,{"content": "application/json"});
-        console.log(response.data.myteams.level_1)
-        console.log(response.data.myteams.level_2)
-        console.log(response.data.myteams.level_3)
-        setLevel_1(response.data.myteams.level_1)
-        setTeam_size(response.data.team_size)
-        setTeam_recharge(response.data.team_recharge)
-        setWithdrawls(response.data.team_withdrawal)
-        setLevel_2(response.data.myteams.level_2)
-        setLevel_3(response.data.myteams.level_3)
+      setUser({
+        invite_code: decoded.invite_code,
+      });
+      const response = await axios.post(
+        "http://139.59.32.207/accounts/my-teams/",
+        { phone_number: decoded.phone_number },
+        { content: "application/json" }
+      );
+      console.log(response.data.myteams.level_1);
+      console.log(response.data.myteams.level_2);
+      console.log(response.data.myteams.level_3);
+      setLevel_1(response.data.myteams.level_1);
+      setTeam_size(response.data.team_size);
+      setTeam_recharge(response.data.team_recharge);
+      setWithdrawls(response.data.team_withdrawal);
+      setLevel_2(response.data.myteams.level_2);
+      setLevel_3(response.data.myteams.level_3);
+    };
+    userDeatil();
+  }, []);
+  const [copy, setCopy] = useState("Copy Link");
+  const handelCopy = () => {
+    setCopy("Copied...");
 
+    navigator.clipboard.writeText(user.invite_code);
 
-        }
-        userDeatil()
-
-    }, [])
-    const [copy , setCopy] = useState("Copy Link");
-    const handelCopy = () => {
-         setCopy("Copied...");
-
-        navigator.clipboard.writeText(user.invite_code);
-
-        setTimeout(() => {
-            setCopy("Copy Link");
-        }, 2000);
-
-
-    }
+    setTimeout(() => {
+      setCopy("Copy Link");
+    }, 2000);
+  };
   return (
     <React.Fragment>
-        <div style={{marginTop:"7rem"}} className="md:w-5/6 w-full mx-auto">
-            <div className="border mx-1 py-4 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-around">
-
-            
-            
-            <div className="teamRecharge flex flex-col item-center justify-center">
-            <h2 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>₹{team_recharge}.00</h2>
-                <p>Team Recharge</p>
-            </div>
-            <div className="teamSize flex flex-col item-center justify-center">
-            <h2 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>{team_size}</h2>
-                <p>Team Size</p>
-            </div>
+      <div style={{ marginTop: "7rem" }} className="md:w-5/6 w-full mx-auto">
+        <div className="border mx-1 py-4 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex justify-around">
+          <div className="teamRecharge flex flex-col item-center justify-center">
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              ₹{team_recharge}.00
+            </h2>
+            <p>Team Recharge</p>
+          </div>
+          <div className="teamSize flex flex-col item-center justify-center">
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+              {team_size}
+            </h2>
+            <p>Team Size</p>
+          </div>
         </div>
-        <h2 className='mb-2 mx-1 text-2xl font-semibold tracking-tight text-gray-900 my-5 dark:text-white'>Invitation Code</h2>
+        <h2 className="mb-2 mx-1 text-2xl font-semibold tracking-tight text-gray-900 my-5 dark:text-white">
+          Invitation Code
+        </h2>
         <div className="border mx-1 py-4 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div className="mb-6 mx-3">
-  <label for="success" className="block mb-2 text-sm font-medium text-emerald-700 dark:text-emerald-500">Invite now</label>
-  <input type="text" disabled id="success" className="bg-emerald-50 border border-emerald-500 text-emerald-900 dark:text-emerald-400 placeholder-emerald-700 dark:placeholder-emerald-500 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 dark:bg-gray-700 dark:border-emerald-500" placeholder={user.invite_code} />
-  </div>
-  <button type="button" className="focus:outline-none text-white mx-3 bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
-  onClick={handelCopy}
-  >{copy}</button>
-
-            </div>
-            <h2 className='mb-2 mx-1 text-2xl font-semibold tracking-tight text-gray-900 my-5 dark:text-white'>Team Member</h2>
-            
-<div className="relative overflow-x-auto mx-1">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <tbody>
-            {level_1.length>0 && level_1.map((member, index) => {
-                return (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        1st
-                    </th>
-                    <td className="px-6 py-4">
-                    {member}
-                    </td>
-                    <td className="px-6 py-4 flex">
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-                    </td>
-                </tr>
-            )})}
-            {level_2.length>0 && level_2.map((member, index) => {
-                return (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        2nd
-                    </th>
-                    <td className="px-6 py-4">
-                        {member}
-                    </td>
-                    <td className="px-6 py-4 flex">
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-                    </td>
-                </tr>
-            )})}
-            {level_3.length>0 && level_3.map((member, index) => {
-                return (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        3rd
-                    </th>
-                    <td className="px-6 py-4">
-                    {member}
-                    </td>
-                    <td className="px-6 py-4 flex">
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-        </svg>
-                    </td>
-                </tr>
-            )})}
-        </tbody>
-    </table>
-</div>
-
+          <div className="mb-6 mx-3">
+            <label
+              for="success"
+              className="block mb-2 text-sm font-medium text-[#00032c] dark:text-[#00032c]"
+            >
+              Invite now
+            </label>
+            <input
+              type="text"
+              disabled
+              id="success"
+              className="bg-[#d4d7fb] border border-[#626481] text-[#00032c] dark:text[#00032c] placeholder-[#00032c] dark:placeholder-[#00032c] text-sm rounded-lg focus:ring-[#d4d7fb] focus:border-[#d4d7fb] block w-full p-2.5 dark:bg-gray-700 dark:border-[#d4d7fb]"
+              placeholder={user.invite_code}
+            />
+          </div>
+          <button
+            type="button"
+            className="focus:outline-none text-white mx-3 bg-[#00032c] hover:bg-[#4951b7] focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
+            onClick={handelCopy}
+          >
+            {copy}
+          </button>
         </div>
+        <h2 className="mb-2 mx-1 text-2xl font-semibold tracking-tight text-gray-900 my-5 dark:text-white">
+          Team Member
+        </h2>
+
+        <div className="relative overflow-x-auto mx-1">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <tbody>
+              {level_1.length > 0 &&
+                level_1.map((member, index) => {
+                  return (
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        1st
+                      </th>
+                      <td className="px-6 py-4">{member}</td>
+                      <td className="px-6 py-4 flex">
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                      </td>
+                    </tr>
+                  );
+                })}
+              {level_2.length > 0 &&
+                level_2.map((member, index) => {
+                  return (
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        2nd
+                      </th>
+                      <td className="px-6 py-4">{member}</td>
+                      <td className="px-6 py-4 flex">
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                      </td>
+                    </tr>
+                  );
+                })}
+              {level_3.length > 0 &&
+                level_3.map((member, index) => {
+                  return (
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        3rd
+                      </th>
+                      <td className="px-6 py-4">{member}</td>
+                      <td className="px-6 py-4 flex">
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg
+                          class="w-4 h-4 text-yellow-300"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="currentColor"
+                          viewBox="0 0 22 20"
+                        >
+                          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </React.Fragment>
-  )
+  );
 }
