d28621e56eea1dd411a990f420a70bc3573a4478
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2023-12-29 19:41:57 +0530

    preFinal Commit

--- server/newsmont/dashboard/urls.py
+++ server/newsmont/dashboard/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('all_products/', views.all_proucts, name='all_proucts'),
     path("purchase_product/", views.purchase_product, name="purchase_product"),
     path('my_income_details/', views.income_details, name='my_income_details'),
-    path("exec_task/", views.exec_task, name="exec_task"),
-    path('task_status/', views.task_status, name='task_status'),
 ]
